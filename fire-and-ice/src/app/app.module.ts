96e807804da841cf2b3d78070f5130f86d351b54
Yong Lin <wangyonglin1999@gmail.com>
2020-07-28 19:02:41 -0400

    feat: added most data display

--- fire-and-ice/src/app/app.module.ts
+++ fire-and-ice/src/app/app.module.ts
@@ -4,24 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { SplashComponent } from './components/splash/splash.component';
 import { RequestrService } from './services/requestr.service';
-import { HttpClient, HttpHandler, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { BooksComponent } from './components/books/books/books.component';
 import { AppComponent } from './components/app/app.component';
 
 @NgModule({
-  declarations: [
-    SplashComponent,
-    BooksComponent,
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule,
-  ],
-  providers: [
-    RequestrService,
-  ],
-  bootstrap: [AppComponent]
+  declarations: [SplashComponent, BooksComponent, AppComponent],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  providers: [RequestrService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
